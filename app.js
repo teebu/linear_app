f554a3319b4751a1569cec0e20adc397682c008d
Y <junkbox0@gmail.com>
2021-03-16 14:50:40 -0700

    fixed err reporting

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ const linearClient = new linear.LinearClient({ 'apiKey': process.env.LINEAR_API_
 
 
 let ref_head = process.env.GITHUB_HEAD_REF // "refs/heads/feature/doc-490-evaluate-pull-request-deployment-of"
-let body = core.getInput('message');
 // let body = 'Hello I am a robot 🤖 [G](https://google.com)'
+let body = core.getInput('message');
 
 try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   const issueId = parse_ref(ref_head)
   createComment(issueId, body)
 } catch(err) {
-  core.setFailed(error.message);
+  core.setFailed(err.message);
 }
 
 // todo: parse title, body, ref of git pull request and get the 'doc-id'
 function parse_ref(ref_head) {
-  // the pull request GITHUB_REF "refs/heads/feature/doc-490-evaluate-pull-request-deployment-of"
+  // the pull request GITHUB_REF "doc-490-evaluate-pull-request-deployment-of"
   console.log('ref_head:', ref_head)
 
   if (!ref_head) return null;
 
-  const re = /(doc-\d+)/i
+  const re = /\b(doc-\d+)\b/i
   console.log(ref_head.match(re))
   return ref_head.match(re) && ref_head.match(re)[0]
 }
